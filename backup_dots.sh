da83bc039cd00d02d64c057eda6dd455dd629f82
Noble EUgene <nobleeugene2005@gmail.com>
2024-07-28 21:33:35 +0100

    new commit

--- backup_dots.sh
+++ backup_dots.sh
@@ -1,5 +1,14 @@
-cp -rv ~/.config/hypr ~/hyprland-dots/config/hypr
+#hyprland config
+cp -rv ~/.config/hypr ~/hyprland-dots/config/
+#waybar config
 cp -rv ~/.config/waybar ~/hyprland-dots/config/
+#wallpapers and pictures
+#clean up first
+rm -rf ~/hyprland-dots/config/Pictures/*
+cp -rv ~/Pictures/* ~/hyprland-dots/config/Pictures
+
+#eww
+cp -rv ~/.config/eww ~/hyprland-dots/config/
 cd ~/hyprland-dots
 echo "Enter commit message"
 read commit_message
